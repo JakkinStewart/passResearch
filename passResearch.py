f66213214fb8286352805af658435dc5a6ebd69f
Jakkin <dracogoyle@gmail.com>
2017-09-01 14:06:23 -0500

    minor update. made a dict of all characters, but not per position yet

--- passResearch.py
+++ passResearch.py
@@ -3,11 +3,12 @@
 
 import codecs
 from sys import argv
+import csv
 
 avg = 0
 count = 0
 passList = []
-
+password_dict = {}
 # Reads everything in as UTF-8 (makes other languages easier to work with)
 for password in codecs.open(argv[1], encoding='utf-8'):
 
@@ -16,6 +17,7 @@ for password in codecs.open(argv[1], encoding='utf-8'):
 
     # Helps calculate average length of password
     if len(password) > 0:
+
         length = len(password)
         avg += length
         count += 1
@@ -24,29 +26,32 @@ for password in codecs.open(argv[1], encoding='utf-8'):
     # Creates a temporary list out of the password
     #templist = list(password)
 
-    # If a password is longer than the length of the dictionary, extend the dictionary
-
-    for i in range(len(password)):
-        #print(passtionary)
-
-        if len(passList) < len(password):
-            difference = len(password) - len(passList)
-            for i in range(difference):
-                passList.append({})
-
-        if password[i] in passList[i]:
-            passList[i][password[i]] += 1
-
+    # Count the number of times a character shows up in a specific position
+    password_list = list(password)
+    for character in password:
+        if character in password_dict:
+            password_dict[character] += 1
         else:
-            tempdict = {password[i]: 1}
-            #passtionary[i].update(tempdict)
-            passList[i] = tempdict
+            password_dict[character] = 1
 
-
-print("Average length of password: %d" % (avg/count))
-
-print(passList)
+print("Average length of password: %d\n" % (avg/count))
+print("Password char dictionary")
 #myprint(passtionary)
+
+print(password_dict)
+
+with open('password.csv', 'w') as f:
+    [f.write('{0},{1}\n'.format(key, value)) for key, value in password_dict.items()]
+
+#with open('password.csv', 'wb') as csv_file:
+#    writer = csv.writer(csv_file)
+#    for key, value in password_dict.items():
+#        writer.writerow([key, value])
+
+#with open('password.csv','wb') as f:
+#    w = csv.writer(f)
+#    w.writerow(password_dict.keys())
+#    w.writerow(password_dict.values())
 
 #for i in range(len(passtionary)):
 #    print("Character: %d" % i)
