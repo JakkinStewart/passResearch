46d2de141cce9a77785b406c4cdcbd7fbaf4be1d
Joshua Jordi <dracogoyle@gmail.com>
2017-09-20 19:58:36 -0500

    with help from @Sidewinder2, the code works

--- passResearch.py
+++ passResearch.py
@@ -6,8 +6,8 @@ from sys import argv
 
 avg = 0
 count = 0
-passList = []
-password_dict = {}
+passList = [{}]
+#password_dict = {}
 
 # Reads everything in as UTF-8 (makes other languages easier to work with)
 for password in codecs.open(argv[1], encoding='utf-8'):
@@ -23,46 +23,26 @@ for password in codecs.open(argv[1], encoding='utf-8'):
         avg += length
         count += 1
 
-    if len(password) > len(passList):
-        for i in range(len(password) - len(passList)):
-            passList += []
-            print(passList)
-
-#         if not passList:
-#             for i in range(len(password)):
-#                 passList.append([])
-#
-#     print(passList)
-#     print("Pass: " + password)
-#     # Creates a temporary list out of the password
-#     # templist = list(password)
-#
-#     # Count the number of times a character shows up in a specific position
-#     try:
-#         for i in range(len(password)):
-#             if password[i] in passList[i]:
-#                 print("password is in passList")
-#                 # passList[i][password[i]] += 1
-#             else:
-#                 print('password is not in passList')
-#                 passList.append({password[i]: 1})
-#                 # passList[i][password[i]] = 1
-#             # if character in password_dict:
-#             #     password_dict[character] += 1
-#             # else:
-#             #     password_dict[character] = 1
-#     except IndexError:
-#         print("IndexError")
-#         for i in range(len(password) - len(passList)):
-#             passList.append([])
-#         print(passList)
+#for password in content:
+    for index in range(0,len(password)):
+        # if a dictionary doesn't exist for that character, make one
+        if len(passList) <= index:
+            passList.append({})
+        # if the character at that index doesn't exist, initialize it
+        if password[index] not in passList[index].keys():
+            (passList[index])[password[index]] = 1
+        # if it does exist, add to it
+        else:
+        #    (passList[index])[password[index]] = (passList[index])[password[index]] + 1
+            (passList[index])[password[index]] += 1
 
 
 print(passList)
 print("Average length of password: %d\n" % (avg/count))
-print("Password char dictionary")
-print(password_dict)
 
-with open('password.csv', 'w') as f:
-    [f.write('{0},{1}\n'.format(key, value)) for key, value in
-     password_dict.items()]
+for thing in passList:
+    print(thing)
+
+#with open('password.csv', 'w') as f:
+#    [f.write('{0},{1}\n'.format(key, value)) for key, value in
+#     passList.items()]
